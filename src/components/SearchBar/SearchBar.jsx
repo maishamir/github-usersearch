e2d1ac3bf3fe5507b079f7c891b2137d45c9f13e
maishamir <45680002+maishamir@users.noreply.github.com>
2024-11-15 22:46:28 -0800

    basic search bar styling

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import "./SearchBar.scss";
+import searchIcon from "../../assets/icon-search.svg"
 
 function SearchBar() {
-  return (
-    <div>SearchBar</div>
-  )
+    return (
+        <form action="" className="searchBar">
+            <input type="text" name="username" id="username" placeholder="Search Github username..." className="searchBar__input" />
+            <img src={searchIcon} alt="searchIcon" className="searchBar__icon"/>
+            <button type="submit" className="searchBar__button">Search</button>
+        </form>
+    );
 }
 
-export default SearchBar
+export default SearchBar;
