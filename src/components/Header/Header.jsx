6189592b9c1b5f25a9ccb49acbb42f18ecdcadcb
maishamir <45680002+maishamir@users.noreply.github.com>
2024-11-15 23:17:50 -0800

    simple header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import "./Header.scss"
+import moonIcon from "../../assets/icon-moon.svg"
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className='header'>
+        <h1 className='header__logo'>devfinder</h1>
+          <div className="header__theme">
+              <p className="header__theme--label">DARK</p>
+              <img src={moonIcon} alt="light theme icon" className='header__theme--icon'/>
+        </div>
+    </header>
   )
 }
 
